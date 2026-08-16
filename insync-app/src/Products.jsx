015e991ab006512165d2eca53de891e899a46a58
nash-dessin <n.whinga@gmail.com>
2026-08-16 23:01:55 +0300

    define ProductCard function

--- insync-app/src/Products.jsx
+++ insync-app/src/Products.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { useStore } from './StoreContext'
+import ProductCard from './ProductCard'
 
 export default function Products() {
 	const [products, setProducts] = useState([])
