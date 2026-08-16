2c7977ff18e32766522ee588a0c6fbed40d685ba
nash-dessin <n.whinga@gmail.com>
2026-08-16 22:05:22 +0300

    add :- navigation bar that displays the no. of items in a cart via reduce()

---
+++ insync-app/src/Navbar.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function Navbar() {
+	const { state } = useStore()
+        let totalQuantity = 0;
+
+        for (const item of state.cart) {
+        totalQuantity += item.qty;
+        }
+
+        const cartCount = totalQuantity;
+	return (
+		<nav className="nav">
+			<div className="nav-left">
+				<Link to="/" className="brand">
+					My Store
+				</Link>
+			</div>
+			<div className="nav-right">
+				<Link to="/products">Products</Link>
+				<Link to="/cart">Cart ({cartCount})</Link>
+				{state.user ? <span className="user">Hello, {state.user.name}</span> : <Link to="/login">Login</Link>}
+			</div>
+		</nav>
+	)
+}
+
