9bfcca55cc16ac4f7e65278fab878fb1fbd563f7
nash-dessin <n.whinga@gmail.com>
2026-08-17 09:22:53 +0300

    add icons for styling in home page

--- src/Home.jsx
+++ src/Home.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 export default function Home() {
 	return (
 		<section className="home">
-			<h1>Welcome to The INSYNC♤ Store</h1>
+			<h1>Welcome to The INSYNC ♤ Store</h1>
 			<p>Browse curated products and style with confidence!</p>
 			<Link to="/products" className="btn-primary">
 				View Products ➤
